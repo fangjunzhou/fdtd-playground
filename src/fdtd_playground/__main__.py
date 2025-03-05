b7cb7409c9f0785d9df9c63d4d4eb1b949f893a7
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-03-05 11:10:29 -0800

    Added cell size, wave speed, and time step to Scene2D

--- src/fdtd_playground/__main__.py
+++ src/fdtd_playground/__main__.py
@@ -12,18 +12,26 @@ def main():
         description="A simple FDTD simulation implemented in Taichi."
     )
     parser.add_argument(
-        "-s",
-        "--size",
-        help="Simulation size.",
+        "-g",
+        "--grid",
+        help="Simulation grid size.",
         type=lambda s: tuple(map(int, s.split(','))),
         default=(512, 512)
+    )
+    parser.add_argument(
+        "-c",
+        "--cell",
+        help="Simulation cell size.",
+        type=float,
+        default=0.01
     )
     args = parser.parse_args()
 
     # Simulation size.
-    SIZE: Tuple[int, int] = args.size
-    disp_buf = ti.field(ti.math.vec3, shape=SIZE)
-    scene = Scene2D(SIZE)
+    grid_size: Tuple[int, int] = args.grid
+    cell_size: float = args.cell
+    disp_buf = ti.field(ti.math.vec3, shape=grid_size)
+    scene = Scene2D(grid_size, cell_size)
 
     @ti.kernel
     def render_p_field():
@@ -31,7 +39,7 @@ def main():
         for i, j in disp_buf:
             disp_buf[i, j] = ti.math.vec3(scene.grid[i, j].p)
 
-    gui = ti.GUI("FDTD Simulation", res=SIZE) # pyright: ignore
+    gui = ti.GUI("FDTD Simulation", res=grid_size) # pyright: ignore
     while gui.running:
         render_p_field()
         gui.set_image(disp_buf)
--- src/fdtd_playground/scene.py
+++ src/fdtd_playground/scene.py
@@ -7,7 +7,9 @@ logger = logging.getLogger(__name__)
 
 @ti.dataclass
 class Cell2D:
+    # Cell pressure.
     p: ti.f32
+    # Staggered velocity
     v: ti.math.vec2
 
 @ti.data_oriented
@@ -16,9 +18,22 @@ class Scene2D:
     grid: ti.StructField
     # Simulation grid size.
     size: Tuple[int, int]
+    # Simulation cell size.
+    dx: ti.f32
+    # Sound wave speed.
+    c: ti.f32
+    # Time step size.
+    dt: ti.f32
 
-    def __init__(self, size: Tuple[int, int]) -> None:
+    def __init__(self,
+                 size: Tuple[int, int],
+                 dx: ti.f32,
+                 c: ti.f32 = 340) -> None:
         self.size = size
+        self.dx = dx
+        self.c = c
+        self.dt = dx / (ti.sqrt(2) * c)
+
         self.grid = Cell2D.field(shape=size)
 
 
@@ -26,7 +41,7 @@ def main():
     # Initialize taichi.
     ti.init(arch=ti.gpu)
     # Construct scene.
-    scene = Scene2D((512, 512))
+    scene = Scene2D((512, 512), 0.01)
     logger.info(f"Scene grid shape: {scene.grid.shape}.")
 
 if __name__ == "__main__":
